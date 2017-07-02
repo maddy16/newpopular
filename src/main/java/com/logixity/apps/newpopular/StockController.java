1846377b6d191359e4d77a24727132c4ce30beff
maddy16 <ahmed.maddy@ymail.com>
2017-07-02 20:19:44 +0500

    Add New Stock Dialog Created

--- src/main/java/com/logixity/apps/newpopular/StockController.java
+++ src/main/java/com/logixity/apps/newpopular/StockController.java
@@ -11,9 +11,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,13 +26,13 @@ import javafx.scene.layout.VBox;
  */
 public class StockController implements Initializable {
 
+    Stage dialogStage;
     @FXML
     private AnchorPane contentPane;
-    
+
     @FXML
     private VBox sideBar;
     ResourceBundle bundle;
-
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -38,11 +42,35 @@ public class StockController implements Initializable {
 
     @FXML
     void newStockBtnClicked(ActionEvent event) throws IOException {
-        toggleButton(event);
-        Main.loadScene("/fxml/newStockScene.fxml", this, contentPane, bundle);
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(StockController.class.getResource("/fxml/newStockScene.fxml"));
+            loader.setController(this);
+            loader.setResources(bundle);
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            dialogStage = new Stage();
+            dialogStage.setTitle("New Stock Entry");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(Main.getMainStage());
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+            dialogStage.resizableProperty().setValue(Boolean.FALSE);
+//            searchByCombo.getItems().add("Service Number");
+//            searchByCombo.getItems().add("CPR Number");
+//            searchByCombo.getSelectionModel().select(0);
+
+//            controller.setPerson(person);
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
     }
-    
+
     void toggleButton(ActionEvent event) {
         resetSideButtons();
         JFXButton btn = (JFXButton) event.getSource();
